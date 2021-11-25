873ea2d24f39078d98e00be83fdd0acd7850b2f5
Holutunde <Holutunde99@gmail.com>
2021-11-25 13:20:38 +0100

    Added more details

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,15 +1,15 @@
+import React from 'react'
+import { useParams, Link } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
 
-import React from "react";
-import { useParams, Link } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
+const MovieDetail = () => {
+  const { id } = useParams()
+  const { loading, response, error } = useFetch(`/${id}`)
 
-const MovieDetail= () => {
-  const { id } = useParams();
-  const { loading, response, error } = useFetch(`/${id}`);
-
-  console.log("Moviedetail", { loading, response, error });
+  console.log('Moviedetail', { loading, response, error })
   return (
     <div>
+      <h3>{response?.title} Details</h3>
       {loading ? (
         <span>Loading...</span>
       ) : error ? (
@@ -21,17 +21,20 @@ const MovieDetail= () => {
               src={`https://image.tmdb.org/t/p/w500${response?.poster_path}`}
               alt=""
             />
-            <h5>{response?.title}</h5>
-            <p>{response?.overview}</p>
+            <h2>Title: {response?.title}</h2>
+            <p>Overview: {response?.overview}</p>
+            <p>Original Language: {response?.original_language}</p>
+            <p>Run Time: {response?.runtime} mins</p>
+            <p>Release Date: {response?.release_date}</p>
             <p>
               Rating: {response?.vote_average} ({response?.vote_count} votes)
             </p>
-            <Link to="/">back to movies</Link>
+            <Link to="/">Go back to movies</Link>
           </div>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default MovieDetail;
+export default MovieDetail
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,39 +1,43 @@
-import React from "react";
-import useFetch from "../hooks/useFetch";
-import { Link } from "react-router-dom";
+import React from 'react'
+import useFetch from '../hooks/useFetch'
+import { Link } from 'react-router-dom'
 
 const Popular = () => {
-  const { loading, response, error } = useFetch("/popular");
+  const { loading, response, error } = useFetch('/popular')
 
-  console.log("popular", { loading, response, error });
+  console.log('popular', { loading, response, error })
   return (
     <div>
       <h3>Popular Movies</h3>
       {loading ? (
         <span>Loading...</span>
       ) : error ? (
-        <span>{error?.message}</span>
+        <spani>{error?.message}</spani>
       ) : (
         <div className="row">
           {response?.results?.map((movie) => {
             return (
-             <Link to={`/movies/${movie.id}`} key={movie.id} className='column'>    
+              <Link
+                to={`/movies/${movie.id}`}
+                key={movie.id}
+                className="column"
+              >
                 <img
                   src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
                   alt=""
-                  />
+                />
                 <h5>{movie?.title}</h5>
                 <p>{movie?.overview}</p>
                 <p>
                   Rating: {movie?.vote_average} ({movie?.vote_count} votes)
                 </p>
-               </Link>
-            );
+              </Link>
+            )
           })}
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Popular;
+export default Popular
